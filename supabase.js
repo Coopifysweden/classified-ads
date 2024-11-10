8c15706b8665b30e4792dbcbdbf2b5a0834fb8c3
Coopifysweden <coopify1@gmail.com>
2024-11-10 13:13:15 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://uqycbzxfzvmkdogrgnpk.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxeWNienhmenZta2RvZ3JnbnBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExODE3NDksImV4cCI6MjA0Njc1Nzc0OX0.zyob-uHgwbUlvTx49S94Oe-PzUvKo5ClKpx_N9xDPUg';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
